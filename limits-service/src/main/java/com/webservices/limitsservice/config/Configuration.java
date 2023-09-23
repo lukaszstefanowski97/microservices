84a4a48b295af9277b0b4b71a0f323aad2c6b3d9
Łukasz Stefanowski <lukasz.stefanowski@lamin.ar>
2023-09-23 14:49:01 +0100

    created limits service

---
+++ limits-service/src/main/java/com/webservices/limitsservice/config/Configuration.java
@@ -0,0 +1,14 @@
+package com.webservices.limitsservice.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "limits-service")
+public class Configuration {
+
+    private int minimum;
+    private int maximum;
+}
---
+++ limits-service/src/main/java/com/webservices/limitsservice/controller/LimitsController.java
@@ -0,0 +1,19 @@
+package com.webservices.limitsservice.controller;
+
+import com.webservices.limitsservice.bean.Limits;
+import com.webservices.limitsservice.config.Configuration;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class LimitsController {
+
+    private Configuration configuration;
+
+    @GetMapping("/limits")
+    public Limits retrieveLimits() {
+        return new Limits(configuration.getMinimum(), configuration.getMaximum());
+    }
+}
