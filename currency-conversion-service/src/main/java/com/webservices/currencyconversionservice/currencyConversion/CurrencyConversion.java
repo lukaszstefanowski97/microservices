036f51faaae7158c8c00aa615a82490ee69737ff
Łukasz Stefanowski <lukasz.stefanowski@lamin.ar>
2023-09-24 14:24:27 +0100

    created currency conversion service

---
+++ currency-conversion-service/src/main/java/com/webservices/currencyconversionservice/currencyConversion/CurrencyConversion.java
@@ -0,0 +1,23 @@
+package com.webservices.currencyconversionservice.currencyConversion;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.math.BigDecimal;
+
+@Data
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class CurrencyConversion {
+
+    private Long id;
+    private String from;
+    private String to;
+    private BigDecimal conversionMultiple;
+    private BigDecimal quantity;
+    private BigDecimal totalCalculatedAmount;
+    private String environment;
+}
