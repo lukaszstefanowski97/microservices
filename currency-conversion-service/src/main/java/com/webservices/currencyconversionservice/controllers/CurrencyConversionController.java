352510718e6c07a9d15e6b1a78e3c6dda916ed4c
Łukasz Stefanowski <lukasz.stefanowski@lamin.ar>
2023-09-24 14:34:16 +0100

    added feign proxy

--- currency-conversion-service/src/main/java/com/webservices/currencyconversionservice/controllers/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/webservices/currencyconversionservice/controllers/CurrencyConversionController.java
@@ -1,33 +1,26 @@
 package com.webservices.currencyconversionservice.controllers;
 
 import com.webservices.currencyconversionservice.currencyConversion.CurrencyConversion;
-import org.springframework.http.ResponseEntity;
+import com.webservices.currencyconversionservice.proxy.CurrencyExchangeProxy;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Objects;
 
 @RestController
+@AllArgsConstructor
 public class CurrencyConversionController {
+
+    private CurrencyExchangeProxy proxy;
 
     @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversion calculateCurrencyConversion(
             @PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
 
-        Map<String, String> uriVariables = new HashMap<>();
-        uriVariables.put("from", from);
-        uriVariables.put("to", to);
-
-        ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate().getForEntity(
-                "http://localhost:8000/currency-exchange/from/{from}/to/{to}",
-                CurrencyConversion.class, uriVariables);
-
-        CurrencyConversion currencyConversion = responseEntity.getBody();
+        CurrencyConversion currencyConversion = proxy.retrieveExchangeValue(from, to);
 
         return new CurrencyConversion(Objects.requireNonNull(currencyConversion).getId(),
                 from, to, currencyConversion.getConversionMultiple(),
