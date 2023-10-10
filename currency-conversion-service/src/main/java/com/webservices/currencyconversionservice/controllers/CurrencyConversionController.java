e9eb2ca536c5cfced50320cbc255b442dbc12335
Łukasz Stefanowski <lukasz.stefanowski@lamin.ar>
2023-10-10 20:06:04 +0200

    added distributed tracing server

--- currency-conversion-service/src/main/java/com/webservices/currencyconversionservice/controllers/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/webservices/currencyconversionservice/controllers/CurrencyConversionController.java
@@ -1,26 +1,35 @@
 package com.webservices.currencyconversionservice.controllers;
 
 import com.webservices.currencyconversionservice.currencyConversion.CurrencyConversion;
-import com.webservices.currencyconversionservice.proxy.CurrencyExchangeProxy;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.Objects;
 
 @RestController
 @AllArgsConstructor
 public class CurrencyConversionController {
 
-    private CurrencyExchangeProxy proxy;
+    private RestTemplate restTemplate;
 
     @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversion calculateCurrencyConversion(
             @PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
 
-        CurrencyConversion currencyConversion = proxy.retrieveExchangeValue(from, to);
+        HashMap<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("from", from);
+        uriVariables.put("to", to);
+
+        ResponseEntity<CurrencyConversion> responseEntity = restTemplate.getForEntity
+                ("http://localhost:8000/currency-exchange/from/{from}/to/{to}",
+                        CurrencyConversion.class, uriVariables);
+        CurrencyConversion currencyConversion = responseEntity.getBody();
 
         return new CurrencyConversion(Objects.requireNonNull(currencyConversion).getId(),
                 from, to, currencyConversion.getConversionMultiple(),
--- currency-exchange-service/src/main/java/com/webservices/currencyexchangeservice/controllers/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/webservices/currencyexchangeservice/controllers/CurrencyExchangeController.java
@@ -5,11 +5,13 @@ import com.webservices.currencyexchangeservice.currencyExchange.CurrencyExchange
 import io.github.resilience4j.retry.annotation.Retry;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @AllArgsConstructor
 public class CurrencyExchangeController {
@@ -20,6 +22,8 @@ public class CurrencyExchangeController {
     @Retry(name = "currency-exchange", fallbackMethod = "hardcodedResponse")
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange retrieveExchangeValue(@Valid @PathVariable String from, @Valid @PathVariable String to) {
+
+        log.info("retrieveExchangeValue called with {} to {}", from, to);
         CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
 
         if (currencyExchange == null) {
